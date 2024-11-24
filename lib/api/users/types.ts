73e24400c78ca799078b59fcb9c6dd049f12d818
javi-saavedra <javiera.saavedra@uc.cl>
2024-11-24 03:19:06 -0300

    fix(users): fix type errors

--- lib/api/users/types.ts
+++ lib/api/users/types.ts
@@ -1,5 +1,5 @@
 import type { z } from 'zod';
 import type * as schemas from './schemas';
 
-export type UserShow = z.infer<typeof schemas.show>;
+export type UserCreate = z.infer<typeof schemas.create>;
 export type UserFields = z.infer<typeof schemas.fields>;
